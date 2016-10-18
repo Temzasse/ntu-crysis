00d7c3f9e14ac3d1f87bd33dbf8b57c9b532428f
DESKTOP-C3NVSO5\Joel Chee <JoelCJA>
2016-10-18 17:03:39 +0800

    Added base page for Report Incident Page.

--- client/components/incident/ReportIncidentForm/index.js
+++ client/components/incident/ReportIncidentForm/index.js
@@ -1,35 +1,65 @@
-import React, { Component, PropTypes } from 'react';
-import shallowCompare from 'react-addons-shallow-compare';
+import React, { PropTypes } from 'react';
+// import { Link } from 'react-router';
 import CSSModules from 'react-css-modules';
 
 // Styles
 import styles from './index.scss';
 
 const propTypes = {
-  something: PropTypes.object,
+  handleArea: PropTypes.func.isRequired,
+  handleReportIncident: PropTypes.func.isRequired,
 };
 
-class Base extends Component {
-  constructor(props) {
-    super(props);
-  }
+const ReportIncidentForm = ({ handleArea, handleReportIncident }) => (
+  <div>
+    <div styleName='ReportIncidentForm'>
+      <div styleName='IncidentField'>
+        <div styleName='c1'>
+          <img id='test1' src='/images/crysis-logo.png' alt='brand logo' height='70' />
+        </div>
+        <table>
+          <tr>
+            <td>Title</td>
+            <td><input /></td>
+          </tr>
+          <tr>
+            <td>Type</td>
+            <td><select name='type'>
+              <option value='Land'>Land</option>
+              <option value='Sea'>Sea</option>
+            </select>
+            </td>
+          </tr>
+          <tr>
+            <td>Long</td>
+            <td><input /></td>
+          </tr>
+          <tr>
+            <td>Lat</td>
+            <td><input /></td>
+          </tr>
+          <tr>
+            <td>Area</td>
+            <td><select name='area' onChange={event => handleArea(event.target.value)}>
+              <option value='Bukit Batok'>Bukit Batok</option>
+              <option value='Choa Chu Kang'>Choa Chu Kang</option>
+            </select>
+            </td>
+          </tr>
+          <tr>
+            <td>Comments</td>
+            <td><textarea rows='4' cols='15' /></td>
+          </tr>
+        </table>
 
-  shouldComponentUpdate(nextProps, nextState) {
-    return shallowCompare(this, nextProps, nextState);
-  }
-
-  render() {
-    return (
-      <div styleName='Base'>
-        Base
+        <center><button onClick={handleReportIncident}>Report</button></center>
       </div>
-    );
-  }
+    </div>
+  </div>
+);
 
-}
+ReportIncidentForm.propTypes = propTypes;
+// ReportIncidentForm.defaultProps = {};
 
-Base.propTypes = propTypes;
-
-// Base.defaultProps = {};
-
-export default CSSModules(Base, styles); // { allowMultiple: true }
+export default
+CSSModules(ReportIncidentForm, styles); // { allowMultiple: true }
