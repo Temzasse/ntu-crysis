606b8f97963d5e6f69265fba570b8cd5bc2a87a4
Teemu Taskula <teemu.taskula@aalto.fi>
2016-10-18 16:46:55 +0800

    Updated client-side websocket implementation

--- client/services/websocket.js
+++ client/services/websocket.js
@@ -5,15 +5,25 @@
 */
 
 // TODO: these could be moved to config file.
-const wsEndpoint = 'crisis';
+const wsPort = 8000;
+const wsEndpoint = 'cms';
 const scheme = window.location.protocol === 'https:' ? 'wss' : 'ws';
-const url = `${window.location.host}/${wsEndpoint}`;
+const url = `localhost:${wsPort}/${wsEndpoint}`;
 const ws = new WebSocket(`${scheme}://${url}`);
 
 
 export const connect = (store) => {
   console.debug('[WEBSOCKET] ====> connect');
-  ws.onmessage = ({ data }) => store.dispatch(data);
+  ws.onmessage = ({ data }) => {
+    console.debug('[WEBSOCKET] ====> receive', data);
+    try {
+      const json = JSON.parse(data);
+      if (data.type && data.payload) store.dispatch(data);
+      else console.error('Websocket data in wrong format!', json);
+    } catch (e) {
+      console.error(e);
+    }
+  };
 };
 
 export const send = (data) => {
