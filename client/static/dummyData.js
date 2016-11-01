b4c1d266bd5c61b7db56b67edbeb883dc4024038
Teemu Taskula <teemu.taskula@aalto.fi>
2016-11-01 11:39:51 +0800

    Added yet another map theme...

--- client/static/dummyData.js
+++ client/static/dummyData.js
@@ -400,6 +400,63 @@ export const mapTheme2 = [
       ]
   }
 ];
+
+export const mapTheme3 = [
+  {
+    "featureType":"all",
+    "elementType":"all",
+    "stylers":[
+      {"invert_lightness":true},
+      {"saturation":20},
+      {"lightness":50},
+      {"gamma":0.4},
+      {"hue":"#00ffee"}
+    ]
+  },
+  {
+    "featureType":"all",
+    "elementType":"geometry",
+    "stylers":[
+      {"visibility":"simplified"}
+    ]
+  },
+  {
+    "featureType":"all",
+    "elementType":"labels",
+    "stylers":[
+      {"visibility":"on"}
+    ]
+  },
+  {
+    "featureType":"administrative",
+    "elementType":"all",
+    "stylers":[
+      {"color":"#ffffff"},
+      {"visibility":"simplified"}
+    ]
+  },
+  {
+    "featureType":"administrative.land_parcel",
+    "elementType":"geometry.stroke",
+    "stylers":[
+      {"visibility":"simplified"}
+    ]
+  },
+  {
+    "featureType":"landscape",
+    "elementType":"all",
+    "stylers":[
+      {"color":"#405769"}
+    ]
+  },
+  {
+    "featureType":"water",
+    "elementType":"geometry.fill",
+    "stylers":[
+      {"color":"#232f3a"}
+    ]
+  }
+];
 /* eslint-enable */
 
 export const mapTheme = [
