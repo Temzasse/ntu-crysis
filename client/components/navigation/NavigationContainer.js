56b61f23e4cb911345a2ab72f1484f459a95bd60
Teemu Taskula <teemu.taskula@aalto.fi>
2016-11-06 16:56:13 +0800

    Fixed typo in nav component

--- client/components/navigation/NavigationContainer.js
+++ client/components/navigation/NavigationContainer.js
@@ -74,7 +74,7 @@ class NavigationContainer extends Component {
       // Determine nav items based on users role
       if (currentUser.role === 'operator') {
         navItems.push(
-          { label: 'Dashboard', to: '/map' },
+          { label: 'Dashboard', to: '/' },
           { label: 'Report Incident', to: '/report-incident' },
           { label: 'Archives', to: '/archives' },
         );
@@ -88,7 +88,7 @@ class NavigationContainer extends Component {
         );
       }
       navItems.push(
-        { label: 'Public Map', to: '/' },
+        { label: 'Public Map', to: '/map' },
         { label: 'Logout', to: '/login', onClick: this.props.doLogout },
       );
     } else {
